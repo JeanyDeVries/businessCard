f49bc846bb1791a510641920129ce33ed7e80ebd
JeanyDeVries <jeanydevries@hotmail.com>
2022-02-08 16:56:57 +0100

    added player movement

--- visitekaartje/game.js
+++ visitekaartje/game.js
@@ -1,5 +1,6 @@
 var playerImg;
 var backgroundImg;
+var hasStarted = false;
 
 window.onload = loadImages();
 
@@ -15,6 +16,7 @@ function loadImages(){
     
     placePlayer();
     placeBackground();
+    checkInput();
 }
 
 function placePlayer(){
@@ -25,7 +27,38 @@ function placePlayer(){
 function placeBackground(){
     var src = document.getElementById("background");
     src.appendChild(backgroundImg);
+}
 
+function checkInput(){
+
+    document.addEventListener("keyup", function(event) 
+    {
+        if(hasStarted) return;
+        if (event.keyCode === 13) {
+            movementPlayer();
+        }
+    });
+
+    document.addEventListener("keyup", function(event) {
+        if (event.keyCode === 32) {
+            jumpPlayer();
+        }
+    });
+}
+
+function movementPlayer(){
+    var pos = 0;
+    hasStarted = true;
+    timer = setInterval(function() {
+        pos++;
+        playerImg.style.left = pos+"px";
+        if( pos == 2000) clearInterval(timer);
+    },25);
+}
+
+function jumpPlayer()
+{
+    
 }
 
 function changePage(){

174aa0f9cb763fc90ce4a9eb2f0bd47465caf85c
Jeany <jeanydevries@hotmail.com>
2022-02-24 14:25:07 +0100

    obstacles added + collision detection

--- visitekaartje/game.js
+++ visitekaartje/game.js
@@ -1,6 +1,5 @@
-var playerImg;
-var backgroundImg;
-var flagImg
+var playerImg, backgroundImg, flagImg;
+var obstacle1Img, obstacle2Img, obstacle3Img, obstacle4Img
 
 var hasStarted = false;
 let isJumping = false
@@ -8,6 +7,8 @@ let isJumping = false
 var jumpHeight = 1;
 let bottomPlayer = 240;
 let timeInAir = 0;
+
+let collisionDistance = 20;
 
 window.onload = start();
 
@@ -20,7 +21,12 @@ function start(){
     placeObstacles();
     
     checkInput();
-}
+    setInterval(function(){   
+        //Check collision every second  
+        if(hasStarted)
+            checkCollision();
+    }, 100);
+    }
 
 function loadImages(){
     playerImg = document.createElement("img");
@@ -36,6 +42,23 @@ function loadImages(){
     flagImg = document.createElement("img");
     flagImg.src = "Images/finishFlag.png";
     flagImg.classList.add('finishFlag');
+
+
+    obstacle1Img = document.createElement("img");
+    obstacle1Img.src = "Images/cacti.png";
+    obstacle1Img.classList.add('obstacle1');
+
+    obstacle2Img = document.createElement("img");
+    obstacle2Img.src = "Images/cacti.png";
+    obstacle2Img.classList.add('obstacle2');
+
+    obstacle3Img = document.createElement("img");
+    obstacle3Img.src = "Images/cacti.png";
+    obstacle3Img.classList.add('obstacle3');
+
+    obstacle4Img = document.createElement("img");
+    obstacle4Img.src = "Images/cacti.png";
+    obstacle4Img.classList.add('obstacle4');
 }
 
 function placePlayer(){
@@ -54,7 +77,17 @@ function placeFinishFlag(){
 }
 
 function placeObstacles(){
+    var src1 = document.getElementById("obstacle1");
+    src1.appendChild(obstacle1Img);
 
+    var src2 = document.getElementById("obstacle2");
+    src2.appendChild(obstacle2Img);
+
+    var src3 = document.getElementById("obstacle3");
+    src3.appendChild(obstacle3Img);
+
+    var src4 = document.getElementById("obstacle4");
+    src4.appendChild(obstacle4Img);
 }
 
 function checkInput(){
@@ -76,7 +109,7 @@ function movementPlayer(){
     var pos = 0;
     hasStarted = true;
     timer = setInterval(function() {
-        pos += 1.5;
+        pos += 2.6;
         playerImg.style.left = pos+"px";
         if( pos == 2000) clearInterval(timer);
     },25);
@@ -110,6 +143,45 @@ function jumpPlayer()
       playerImg.style.bottom = position + 'px'
     },20)
 }
+
+function checkCollision(){
+    const distanceObstacle1 = getDistanceBetweenElements(
+        obstacle1Img,
+        playerImg
+    );
+    const distanceObstacle2 = getDistanceBetweenElements(
+        obstacle2Img,
+        playerImg
+    );
+    const distanceObstacle3 = getDistanceBetweenElements(
+        obstacle3Img,
+        playerImg
+    );
+    const distanceObstacle4 = getDistanceBetweenElements(
+        obstacle4Img,
+        playerImg
+    );
+      
+    if(distanceObstacle1 < collisionDistance || distanceObstacle2 < collisionDistance
+        || distanceObstacle3 < collisionDistance || distanceObstacle4 < collisionDistance){
+            console.log("game over")
+    }
+}
+
+function getPositionAtCenter(element) {
+    const {top, left, width, height} = element.getBoundingClientRect();
+    return {
+      x: left + width / 2,
+      y: top + height / 2
+    };
+  }
+ 
+ function getDistanceBetweenElements(a, b) {
+   const aPosition = getPositionAtCenter(a);
+   const bPosition = getPositionAtCenter(b);
+
+   return Math.hypot(aPosition.x - bPosition.x, aPosition.y - bPosition.y);  
+ }
 
 function vhtopx(vh) {
     return document.documentElement.clientHeight * (vh/100)
